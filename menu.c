12b647a54b61da599e0fd1c107e574676e24c2e4
Joanna <21012167+ruffledpigeon@users.noreply.github.com>
2024-03-28 22:18:43 -0400

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------
+Joanna Ni 3/26/2024
+1011 01 Lab#9
+
+menu.c - prints pizza name and ingredients
+-----------------------------------------------------*/
+
+#include "defs.h"
+
+/*------------------------printPizzas--------------------------------
+- prints the array of pizza names and their ingredients
+- parameters: 
+- returns: none
+-------------------------------------------------------------------*/
+void printPizzas(pizza_t menu[], int numPizza){
+
+	printf("\n\n%25s", "PIZZA MENU");
+	printf("\n\n%-20sIngredients\n", "Pizza");
+	printf("----------------------------------------------------\n\n");
+	
+	for (int i = 0; i < numPizza; i++) {
+		printf("%-20s%-70s\n", menu[i].pizzaName, menu[i].ingredients);
+	}
+}
